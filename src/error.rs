cec85b14c688d853a513d6bb880021b0856f03d9
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-01-15 01:51:31 -0500

    Added documentation

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,14 @@
+//! This module contains all enums to the define errors that could occur at run time.
+
+/// Error enum defines all the possible errors that can occur on run time.
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error{
+    /// IndexOutOfRange is thrown when a retrieval function(`.get(index)` or `list[index]`) is run with an index that does not exist within the function co-domain.
     IndexOutOfRange,
+
+    /// Overflow is thrown when data type fails to add another element
     Overflow,
+
+    /// InvalidSize is thrown when a data structure or type is too large to be converted into any of the cyclic data type variant
     InvalidSize
 }
--- src/list/mod.rs
+++ src/list/mod.rs
@@ -1,15 +1,113 @@
+//! The list module contains a series of structs to create cyclic lists and their utility functionalities.
+//! 
+//! Cyclic List Example
+//! ```text
+//! 1 ↔ 2 ↔ ... ↔ n
+//! ↑             ↑
+//! └─────────────┘
+//! ```
+//! Note: Even though the diagram uses arrow to denote the relationship between nodes/elements; implying a linked node structure - however, the implementation in this crate uses arrays.
+//! 
+//! As result, a [`List<T>`] struct can be sized (assuming that `T` is also sized). Allowing the `List` to exist on the stack.
 use std::{ops::{Index, IndexMut}, fmt::{Display, Debug}, collections::LinkedList, mem};
 use std::convert::TryFrom;
+
+use std::iter::FromIterator;
 
 use crate::{CyclicList, error::Error};
 
 use self::iterator::Iter;
 
-pub mod iterator;
+mod iterator;
 
 #[cfg(test)]
 mod tests;
 
+/// `List` is the `struct` used to define the state of a cyclic List
+/// 
+/// # Generics
+/// List types are derived using 3 generics.
+/// 
+/// 1. `const SIZE: usize`
+/// 
+/// SIZE is a generic constant [^note] that defines the maximum size of the list
+/// 
+/// 2. `T: Sized`
+/// 
+/// T is the type of element stored in the list
+/// 
+/// 3. `const WRITE_OVER: bool>`
+/// 
+/// WRITE_OVER is a generic constant [^note] that is used to determine if elements should be over written on overflow
+/// 
+/// # Creating Lists
+/// 
+/// List can be created in a couple of ways.
+/// 
+/// 1. Empty list
+/// 
+/// Empty list are created using the [`Default`] trait implementation for List.
+/// 
+/// ```
+/// # use cyclic_data_types::list::List;
+/// # const SIZE: usize = 5;
+/// let list: List<SIZE, i64, false> = List::default();
+/// 
+/// assert_eq!(list.len(), 0);
+/// ```
+/// 2. From Array
+/// 
+/// List can can also be from arrays. The maximum size of the list is the same size as the array. This is done using the [`From<[SIZE; T]`] trait implementation for List.
+/// 
+/// ```
+/// # use cyclic_data_types::list::List;
+/// # const SIZE: usize = 5;
+/// let list: List<SIZE, i64, false> = [1i64,2i64,3i64,4i64,5i64].into();
+/// #
+/// # assert_eq!(list.len(), 5);
+/// # assert_eq!(list[0usize], 1i64);
+/// # assert_eq!(list[1usize], 2i64);
+/// # assert_eq!(list[2usize], 3i64);
+/// # assert_eq!(list[3usize], 4i64);
+/// # assert_eq!(list[4usize], 5i64);
+/// ```
+/// 
+/// 3. From Vectors, Linked Lists and Iterators
+/// 
+/// Since collections (Vectors, Linked Lists and Iterators) cannot guarantee a size at compile time - the conversion is not always guaranteed to succeed. This occurs when collection is larger than the List variant. As a result, the new cyclic list cannot be created without resulting in a [`Error::Overflow`]. This can be resolved by either making sure the collection is at max the same size as the cyclic list variant or the cyclic list variant permits `WRITE_OVER`.
+/// 
+/// Therefore, the [`TryFrom`] trait implementation of List must be used.
+/// 
+/// Example of a successful conversion
+/// ```
+/// # use cyclic_data_types::list::List;
+/// const SIZE: usize = 5;
+/// let list: List<SIZE, i64, false> = List::try_from(vec![1i64,2i64,3i64,4i64,5i64])
+///     .unwrap();
+/// #
+/// # assert_eq!(list.len(), 5);
+/// # assert_eq!(list, [1i64,2i64,3i64,4i64,5i64].into());
+/// ```
+/// ```
+/// # use cyclic_data_types::list::List;
+/// const SIZE: usize = 5;
+/// let list: List<SIZE, i64, true> = vec![1i64,2i64,3i64,4i64,5i64,6i64].try_into()
+///     .unwrap();
+/// #
+/// # assert_eq!(list.len(), 5);
+/// # assert_eq!(list, [2i64,3i64,4i64,5i64,6i64].into());
+/// ```
+/// Example of a failed conversion
+/// ```
+/// # use cyclic_data_types::list::List;
+/// # use cyclic_data_types::error::Error;
+/// const SIZE: usize = 5;
+/// let list: Result<List<SIZE, i64, false>, Error> = List::try_from(vec![1i64,2i64,3i64,4i64,5i64,6i64]);
+/// 
+/// assert_eq!(list, Err(Error::Overflow))
+/// ```
+/// 
+/// [note]: [Generic Constraints](https://rust-lang.github.io/rfcs/2000-const-generics.html)
 #[derive(Eq, PartialEq, Default)]
 pub struct List<const SIZE: usize, T: Sized, const WRITE_OVER: bool>{
     list: CyclicList<SIZE, T, WRITE_OVER>
@@ -18,7 +116,9 @@ pub struct List<const SIZE: usize, T: Sized, const WRITE_OVER: bool>{
 impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
     /// Returns the number of elements in the list
     /// 
-    /// ```rust
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
     /// let mut list: List<SIZE, i64, false>  = List::default();
     /// 
     /// assert_eq!(list.len(), 0);
@@ -36,6 +136,48 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
     //     todo!();
     // }
 
+    /// Pushes a new element to the back of the list. This operation is done in `O(1)`.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = List::default();
+    /// 
+    /// assert!(list.push_back(1).is_ok());
+    /// ```
+    /// 
+    /// If the list is full - the list has two options based on the `WRITE_OVER` flag.
+    /// 1. `WRITE_OVER = true`
+    /// 
+    /// The first element is written over and dropped. Meaning the first element is no longer in the list.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, true> = [1,2,3,4,5].into();
+    /// 
+    /// assert!(list.push_back(6).is_ok());
+    /// 
+    /// assert_eq!(list, [2,3,4,5,6].into());
+    /// ```
+    /// 
+    /// 2. `WRITE_OVER = false`
+    /// 
+    /// The new element isn't added to the list. Resulting in no change to the state of the list.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// 
+    /// assert!(list.push_back(6).is_err());
+    /// 
+    /// assert_eq!(list, [1,2,3,4,5].into());
+    /// ```
+    /// 
+    /// # Returns
+    /// * Self if the push was successful
+    /// * [Error] if List is full and the `WRITE_OVER` flag is set to `false`
     pub fn push_back(&mut self, elem: T) -> Result<&mut Self, Error> {
         if self.len() + 1 > SIZE && !WRITE_OVER {
             return Err(Error::Overflow)
@@ -67,6 +209,48 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
         Ok(self)
     }
 
+    /// Pushes a new element to the front of the list. This operation is done in `O(1)`.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = List::default();
+    /// 
+    /// assert!(list.push_front(1).is_ok());
+    /// ```
+    /// 
+    /// If the list is full - the list has two options based on the `WRITE_OVER` flag.
+    /// 1. `WRITE_OVER = true`
+    /// 
+    /// The last element is written over and dropped. Meaning the last element is no longer in the list.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, true> = [1,2,3,4,5].into();
+    /// 
+    /// assert!(list.push_front(0).is_ok());
+    /// 
+    /// assert_eq!(list, [0,1,2,3,4].into());
+    /// ```
+    /// 
+    /// 2. `WRITE_OVER = false`
+    /// 
+    /// The new element isn't added to the list. Resulting in no change to the state of the list.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// 
+    /// assert!(list.push_front(0).is_err());
+    /// 
+    /// assert_eq!(list, [1,2,3,4,5].into());
+    /// ```
+    /// 
+    /// # Returns
+    /// * Self if the push was successful
+    /// * [Error] if List is full and the `WRITE_OVER` flag is set to `false`
     pub fn push_front(&mut self, elem: T) -> Result<&mut Self, Error> {
         if self.len() + 1 > SIZE && !WRITE_OVER {
             return Err(Error::Overflow)
@@ -94,6 +278,52 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
         Ok(self)
     }
 
+    /// returns a reference to an element in the list at provided index.
+    /// 
+    /// The get element retrieval works similarly to a cyclic list and python lists. Where, the list loop backs to the beginning of the list when the index is greater than the size of the list; and the list can be accessed from the end of the list using negative integers.
+    /// 
+    /// Therefore, let the following be a cyclic list.
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// #
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// ```
+    /// 
+    /// The following is equivalent in retrieving the last element
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// # let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// assert_eq!(list.get(4), Some(&5));
+    /// assert_eq!(list.get(4), list.get(9));
+    /// assert_eq!(list.get(4), list.get(-1));
+    /// ```
+    /// 
+    /// The following is equivalent in retrieving the first element
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// # let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// assert_eq!(list.get(0), Some(&1));
+    /// assert_eq!(list.get(0), list.get(5));
+    /// assert_eq!(list.get(0), list.get(-5));
+    /// ```
+    /// 
+    /// # Return
+    /// * `None` if the list is empty
+    /// * `Some(T)` if the list has at least one element
+    /// 
+    /// # Note
+    /// 
+    /// If certain constraints on the index is met. Then the following element retrievals are recommended over `.get(index)`.
+    /// 
+    /// If, i ∈ [0, list.len()) then, `list.get(i)` = `list[i as usize]`
+    /// If, i ∈ [-1 * list.len(), list.len()) then, `list.get(i)` = `list[i as isize]`
+    /// 
+    /// This is because the `Index<usize>` and `Index<isize>` are defined with let checks - with the assumption that certain guarantees on the index are provided on run time.
+    /// 
+    /// Such that, im(`Self::Index<usize>`) ⊆ im(`Self::Index<isize>`) ⊆ im(`Self::get(&self, isize)`).
     pub fn get(&self, index: isize) -> Option<&T> {
         if self.len() == 0 {
             return None
@@ -115,6 +345,62 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
     }
     */
 
+    /// returns a mutable reference to an element in the list at provided index.
+    /// 
+    /// The get_mut element retrieval works similarly to a cyclic list and python lists. Where, the list loop backs to the beginning of the list when the index is greater than the size of the list; and the list can be accessed from the end of the list using negative integers.
+    /// 
+    /// Therefore, let the following be a cyclic list.
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// #
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// ```
+    /// 
+    /// The following is equivalent in retrieving last element
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// # let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// assert_eq!(list.get_mut(4), Some(&mut 5));
+    /// assert_eq!(list.get_mut(9), Some(&mut 5));
+    /// assert_eq!(list.get_mut(-1), Some(&mut 5));
+    /// ```
+    /// 
+    /// The following is equivalent in the retrieving first element
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// # let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// assert_eq!(list.get_mut(0), Some(&mut 1));
+    /// assert_eq!(list.get_mut(5), Some(&mut 1));
+    /// assert_eq!(list.get_mut(-5), Some(&mut 1));
+    /// ```
+    /// 
+    /// The following can be used to update an element
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// # let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// *list.get_mut(0).unwrap() = 6;
+    /// 
+    /// assert_eq!(list.get(0), Some(&6));
+    /// ```
+    /// 
+    /// # Return
+    /// * `None` if the list is empty
+    /// * `Some(T)` if the list has at least one element
+    /// 
+    /// # Note
+    /// 
+    /// If certain constraints on the index is met. Then the following element retrievals are recommended over `.get(index)`.
+    /// 
+    /// If, i ∈ [0, list.len()) then, `list.get(i)` = `list[i as usize]`
+    /// If, i ∈ [-1 * list.len(), list.len()) then, `list.get(i)` = `list[i as isize]`
+    /// 
+    /// This is because the `Index<usize>` and `Index<isize>` are defined with let checks - with the assumption that certain guarantees on the index are provided on run time.
+    /// 
+    /// Such that, im(`Self::Index<usize>`) ⊆ im(`Self::Index<isize>`) ⊆ im(`Self::get_mut(&self, isize)`).
     pub fn get_mut(&mut self, index: isize) -> Option<&mut T> {
         if self.len() == 0 {
             return None
@@ -137,6 +423,23 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
         todo!()
     }
     */
+
+    /// Removes the last element from the list and returns removed element. This occurs in `O(1)`
+    ///
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// 
+    /// assert_eq!(list.remove_back(), Some(5));
+    /// 
+    /// assert_eq!(list.len(), 4);
+    /// assert_eq!(list, vec![1,2,3,4].try_into().unwrap());
+    /// ```
+    /// 
+    /// # Return
+    /// * `Some(last element in list)` if `list.len()` > 0
+    /// * `None` if `list.len()` = 0
     pub fn remove_back(&mut self) -> Option<T> {
         if self.len() == 0 {
             return None
@@ -157,6 +460,22 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
         Some(value.unwrap())
     }
 
+    /// Removes the first element from the list and returns removed element. This occurs in `O(1)`
+    ///
+    /// ```
+    /// # use cyclic_data_types::list::List;
+    /// # const SIZE: usize = 5;
+    /// let mut list : List<SIZE, i64, false> = [1,2,3,4,5].into();
+    /// 
+    /// assert_eq!(list.remove_front(), Some(1));
+    /// 
+    /// assert_eq!(list.len(), 4);
+    /// assert_eq!(list, vec![2,3,4,5].try_into().unwrap());
+    /// ```
+    /// 
+    /// # Return
+    /// * `Some(last element in list)` if `list.len()` > 0
+    /// * `None` if `list.len()` = 0
     pub fn remove_front(&mut self) -> Option<T> {
         if self.len() == 0 {
             return None
@@ -183,10 +502,12 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> List<SIZE, T, WRITE_OVER> {
     //     todo!();
     // }
 
+    /// Creates an iterator object that iterates over the elements in the list
     pub fn iter(&self) -> Iter<SIZE, T, WRITE_OVER> where Self: Sized {
         Iter::new(self)
     }
 
+    /// Creates a iterator object over the list. In which, each element in the iterator can be updated.
     pub fn iter_mut(&mut self) -> Iter<SIZE, T, WRITE_OVER> where Self: Sized {
         Iter::new(self)
     }
