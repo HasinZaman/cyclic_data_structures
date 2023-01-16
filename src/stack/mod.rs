74d84a0893da9def874496f50d3b6edaae36c7b6
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-01-16 15:57:52 -0500

    Update mod.rs

--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -1,6 +1,6 @@
 //! The list module contains a series of structs to create stacks and their utility functionalities using cyclic lists.
 
-use std::{fmt::{Display, Debug}, collections::LinkedList};
+use std::{fmt::{Display, Debug}, collections::LinkedList, ops::{Deref, DerefMut}};
 
 use crate::{list::List, error::Error};
 
@@ -34,9 +34,61 @@ mod tests;
 /// ```
 /// # use cyclic_data_types::stack::Stack;
 /// # const SIZE: usize = 5;
-/// let list: Stack<SIZE, i64, false> = Stack::default();
+/// let stack: Stack<SIZE, i64, false> = Stack::default();
 /// 
-/// assert_eq!(list.len(), 0);
+/// assert_eq!(stack.len(), 0);
+/// ```
+/// 
+/// 2. From Array
+/// 
+/// Stack can also be derived from arrays. The maximum size of the stack is the same size as the array. This is done using the [`From<[SIZE; T]`] trait implementation for List.
+/// 
+/// ```
+/// # use cyclic_data_types::stack::Stack;
+/// # const SIZE: usize = 5;
+/// let stack: Stack<SIZE, i64, false> = [1i64,2i64,3i64,4i64,5i64].into();
+/// #
+/// # assert_eq!(stack.len(), 5);
+/// # assert_eq!(stack.read(5).unwrap(), &1i64);
+/// # assert_eq!(stack.read(4).unwrap(), &2i64);
+/// # assert_eq!(stack.read(3).unwrap(), &3i64);
+/// # assert_eq!(stack.read(2).unwrap(), &4i64);
+/// # assert_eq!(stack.read(0).unwrap(), &5i64);
+/// ```
+/// 
+/// 3. From Vectors, Linked Lists and Iterators
+/// 
+/// Since collections (Vectors, Linked Lists and Iterators) cannot guarantee a size at compile time - the conversion is not always guaranteed to succeed. This occurs when collection is larger than the stack variant. As a result, the new stack cannot be created without resulting in a [`Error::Overflow`]. This can be resolved by either making sure the collection is at max the same size as the stack variant or the cyclic list variant permits `WRITE_OVER`.
+/// 
+/// Therefore, the [`TryFrom`] trait implementation of stack must be used.
+/// 
+/// Example of a successful conversion
+/// ```
+/// # use cyclic_data_types::stack::Stack;
+/// const SIZE: usize = 5;
+/// let stack: Stack<SIZE, i64, false> = Stack::try_from(vec![1i64,2i64,3i64,4i64,5i64])
+///     .unwrap();
+/// #
+/// # assert_eq!(stack.len(), 5);
+/// # assert_eq!(stack, [1i64,2i64,3i64,4i64,5i64].into());
+/// ```
+/// ```
+/// # use cyclic_data_types::stack::Stack;
+/// const SIZE: usize = 5;
+/// let stack: Stack<SIZE, i64, true> = vec![1i64,2i64,3i64,4i64,5i64,6i64].try_into()
+///     .unwrap();
+/// #
+/// # assert_eq!(stack.len(), 5);
+/// # assert_eq!(stack, [2i64,3i64,4i64,5i64,6i64].into());
+/// ```
+/// Example of a failed conversion
+/// ```
+/// # use cyclic_data_types::stack::Stack;
+/// # use cyclic_data_types::error::Error;
+/// const SIZE: usize = 5;
+/// let stack: Result<Stack<SIZE, i64, false>, Error> = Stack::try_from(vec![1i64,2i64,3i64,4i64,5i64,6i64]);
+/// 
+/// assert_eq!(stack, Err(Error::Overflow))
 /// ```
 /// 
 /// WRITE_OVER is a generic constant [^note] that is used to determine if elements should be over written on overflow
@@ -45,10 +97,39 @@ mod tests;
 pub struct Stack<const SIZE: usize, T, const WRITE_OVER: bool> (List<SIZE, T, WRITE_OVER>);
 
 impl<const SIZE: usize, T, const WRITE_OVER: bool> Stack<SIZE, T, WRITE_OVER> {
+    /// Returns the number of elements in the list.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::stack::Stack;
+    /// # const SIZE: usize = 5;
+    /// let mut stack: Stack<SIZE, i64, false> = Stack::default();
+    /// 
+    /// assert_eq!(stack.len(), 0);
+    /// 
+    /// assert!(stack.push(1).is_ok());
+    /// 
+    /// assert_eq!(stack.len(), 1);
+    /// ```
     pub fn len(&self) -> usize {
         self.0.len()
     }
 
+    /// Pushes an element to the end of the stack.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::stack::Stack;
+    /// # const SIZE: usize = 5;
+    /// 
+    /// let mut stack: Stack<SIZE, i64, false> = Stack::default();
+    /// 
+    /// assert!(stack.push(1).is_ok());
+    /// 
+    /// assert!(stack.push(2).is_ok());
+    /// 
+    /// # assert_eq!(stack.len(), 2);
+    /// # assert_eq!(stack.read(0).unwrap(), 2);
+    /// # assert_eq!(stack.read(1).unwrap(), 1);
+    /// ```
     pub fn push(&mut self, elem: T) -> Result<&mut Self, Error>{
         match self.0.push_back(elem){
             Ok(_) => Ok(self),
@@ -56,14 +137,51 @@ impl<const SIZE: usize, T, const WRITE_OVER: bool> Stack<SIZE, T, WRITE_OVER> {
         }
     }
     
+    /// Returns a reference to the top most element of the stack.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::stack::Stack;
+    /// # const SIZE: usize = 5;
+    /// 
+    /// let mut stack: Stack<SIZE, i64, false> = vec![1,2,3].try_into().unwrap();
+    /// 
+    /// # assert_eq!(stack.len(), 3);
+    /// assert_eq!(stack.peek(), Some(&3));
+    /// ```
     pub fn peek(&self) -> Option<&T> {
         self.0.get(-1)
     }
     
+    /// Returns the top most element of the stack - after removing said element from the stack.
+    /// 
+    /// ```
+    /// # use cyclic_data_types::stack::Stack;
+    /// # const SIZE: usize = 5;
+    /// 
+    /// let mut stack: Stack<SIZE, i64, false> = vec![1,2,3].try_into().unwrap();
+    /// 
+    /// # assert_eq!(stack.len(), 3);
+    /// assert_eq!(stack.pop(), Some(3));
+    /// ```
     pub fn pop(&mut self) -> Option<T> {
         self.0.remove_back()
     }
     
+    /// Returns a reference to an element in the stack using an index (relative to the top of the stack).
+    /// 
+    /// ```
+    /// # use cyclic_data_types::stack::Stack;
+    /// # const SIZE: usize = 5;
+    /// 
+    /// let mut stack: Stack<SIZE, i64, false> = vec![1,2].try_into().unwrap();
+    /// 
+    /// # assert_eq!(stack.len(), 2);
+    /// assert_eq!(stack.read(0).unwrap(), 2);
+    /// assert_eq!(stack.read(1).unwrap(), 1);
+    /// ```
+    /// 
+    /// # Return
+    /// read returns [`Error`] if the index is out range of the stack. Otherwise the method returns a reference of element at index.
     pub fn read(&self, index: usize) -> Result<&T, Error> {
         let len = self.0.len();
 
@@ -174,5 +292,19 @@ impl<const STACK_SIZE: usize, T> From<Stack<STACK_SIZE, T, true>> for Stack<STAC
 impl<const STACK_SIZE: usize, T> From<Stack<STACK_SIZE, T, false>> for Stack<STACK_SIZE, T, true> {
     fn from(value: Stack<STACK_SIZE, T, false>) -> Self {
         Self(value.0.into())
+    }
+}
+
+impl<const STACK_SIZE: usize, T, const WRITE_OVER: bool> Deref for Stack<STACK_SIZE, T, WRITE_OVER> {
+    type Target = List<STACK_SIZE, T, WRITE_OVER>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<const STACK_SIZE: usize, T, const WRITE_OVER: bool> DerefMut for Stack<STACK_SIZE, T, WRITE_OVER> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
     }
 }
